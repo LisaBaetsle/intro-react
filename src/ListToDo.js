376bb96795965d713ebbf56f0f5d56b2bed78b88
Lisa Baetsle <lisa_baetsle@hotmail.com>
2020-10-21 11:41:40 +0200

    adding a state hook

--- src/ListToDo.js
+++ src/ListToDo.js
@@ -1,21 +1,18 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const ListToDo = () => {
+export default function ListToDo() {
+  const initialTodos = ["Learn React", "Go Rollerskating"];
+  const [todos, setTodos] = useState(initialTodos);
   return (
-    <div className="todo-list">
-        <h2>My to do list</h2>
-        <input 
-          type="checkbox" 
-        />
-        <label htmlFor="todo1"> Learn React</label>
-        <br />
-        <input 
-          type="checkbox" 
-        />
-        <label htmlFor="todo2"> Go roller skating</label>
-        <br />
-      </div>
+    <ul>
+      {todos.map((todo) => (
+        <li>
+          <input type="checkbox" /> {todo}
+        </li>
+      ))}
+    </ul>
   );
 }
 
-export default ListToDo;
+//The map() function is used to iterate over an array and manipulate or change data items. In React, the map() function is most commonly used for rendering a list of data to the DOM.
+

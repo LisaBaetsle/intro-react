65afe58e07f6e50755e10bfb58176681d85c480c
Lisa Baetsle <lisa_baetsle@hotmail.com>
2020-10-22 12:00:11 +0200

    adding an onclick function

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 export default function Form() {
+  const inputRef = useRef(); //Make a variable to use one the input so that we can get the user input (input reference)
+
+  function toggleToDo() {
+    const inputElement = inputRef.current; //Take the current user input
+    console.log(inputElement.value); // console log the value of the current user input
+
+  }
+
   return (
     <form>
       <input
       type="text"
+      ref={ inputRef } //variable of the toggleToDo function to get the user input
       placeholder="add some stuff to do"
       />
       <br />
-      <button> Add stuff </button>
+      <button onClick={toggleToDo}> Add stuff </button>
     </form>
      
   );
